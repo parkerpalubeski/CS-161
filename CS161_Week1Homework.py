01201ec8757944eeaab1f7deec297c6498468056
parker <ptpski@gmail.com>
2025-01-12 17:39:22 -0800

    CIS 161 - 1st Week Assignment (Part 1-2 Complete)

--- CS161_Week1Homework.py
+++ CS161_Week1Homework.py
@@ -1,1 +1,15 @@
-#Extra test. Hello!
+#CS 161 Week 1 Assignment
+
+#Part 1
+sibling_type = "Sister" #I do not have any pets, so I am using my sister instead
+sibling_name = "Hannah"
+print(f"I have a {sibling_type} and her name is {sibling_name}.")
+#Part 2
+name = input("Enter your first name: ")
+age = int(input("Enter your current age: "))
+savings = int(input("Enter your yearly savings: "))
+print(f"Hello {name}! You are currently {age} years old.")
+print(f"In 10 years, you will be {age + 10} years old.")
+print(f"If you save {savings} each year, in 5 years you will have saved ${savings * 5}")
+print(f"Your average monthly savings is ${savings // 12:.2f}.") #The sample output had the results include cents.
+#Part 3
