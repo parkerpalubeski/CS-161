b3e68c0cdf39f18a3667b6b155e690c670c62ce7
parker <ptpski@gmail.com>
2025-01-26 21:15:04 -0800

    CIS 161 - 3rd Week Assignment

---
+++ CS161_Week3Homework.py
@@ -0,0 +1,27 @@
+#CS 161 Week 3 Assignment
+
+#1
+
+x = input("What is your name? ")
+print ("Hello " + x)
+
+#2
+
+#y = input ("What is your age? ")
+#print (y + 5)
+
+#This produces an error because the input command assigns the input to a string by default. When the program runs y + 5, it is attempting to add a integer to a string which it can't do.
+
+y = int(input("What is your age? "))
+print(y + 5)
+
+#3
+z = int(input("How many years to add? "))
+print("In " + str(z) + " years, you will be " + str(y + z))
+
+#4
+hours = float(input("Enter the number of hours worked: "))
+wage = float(input("Enter your hourly wage, without the $: "))
+
+print("Your gross pay this week is $ " + str(hours * wage))
+print("Your estimated annual gross pay will be $ " + str({hours * wage * 50}))
