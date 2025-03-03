484cc8b0fc0d7d6fd35f861129686e202a5ae963
parker <ptpski@gmail.com>
2025-03-02 23:01:40 -0800

    Week 8 Assignment

--- CS_161_Week8Homework.py
+++ CS_161_Week8Homework.py
@@ -56,7 +56,6 @@ def factorial_recursive(num):
 def factorial_iterative(num):
     for i in range(1, num):
         num = num * i
-        print(num)
     return num
 
 integer = int(input("Enter a number "))
